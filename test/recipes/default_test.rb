41cbca1edec3887f9a59f46af4c726d1d51a1ab0
jorge <jorge@grrrr.com>
2017-06-08 20:08:15 +0200

    se comprueba el control de property y pertenece a root

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -17,3 +17,7 @@ end
 describe package('tree') do
    it { should be_installed }
 end
+describe file('/etc/motd') do
+  its('content') { should match(/Property of/) }
+  it { should be_owned_by 'root' }
+end
