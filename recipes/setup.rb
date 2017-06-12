3a69650823a6cde96cba58be00b349534fc619f0
jorge <jorge@grrrr.com>
2017-06-12 17:59:59 +0200

    version 0.2.0

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,6 +5,14 @@ package 'tree' do
   action :install
 end
 file '/etc/motd' do
-  content 'Property of Jorge
-'
+  content "Property of Jorge
+
+IPADDRESS: #{node['ipadrdress']}
+HOSTNAME: #{node['hostname']}
+MEMORY: #{node['memory']['total']}
+CPU: #{node['cpu']['0']['mhz']}
+"
+mode '0644'
+owner 'root'
+group 'root'
 end
